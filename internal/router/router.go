ea742df4590061199627b4638afc7bbe27316dfb
Frederico Zolio <fredzolio@live.com>
2025-09-05 14:04:57 -0300

    feat: adicionar verificação de permissões para visualização de inscrições
    - Implementar lógica de autorização nos manipuladores de inscrições, permitindo que apenas administradores e usuários correspondentes ao CPF possam visualizar suas inscrições.
    - Atualizar a documentação OpenAPI para incluir novos códigos de resposta 403 (Acesso negado) nas rotas relevantes.
    - Adicionar middleware para extrair contexto do usuário dos headers, melhorando a segurança e a gestão de permissões nas rotas da API.

--- internal/router/router.go
+++ internal/router/router.go
@@ -36,6 +36,9 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	apiV1 := r.Group("/api/v1")
 	// // Aplicar middleware de autenticação para proteger todas as rotas da API
 	// apiV1.Use(middlewares.AuthMiddleware())
+	
+	// Middleware para extrair contexto do usuário dos headers injetados pelo Istio
+	apiV1.Use(middlewares.ExtractUserContext())
 
 	// Inicializando repositórios
 	cursoRepo := repository.NewCursoRepository(db)
