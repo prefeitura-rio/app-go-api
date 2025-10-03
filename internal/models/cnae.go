3d5644738b44b25d9f4da38c1e05c2312939b4f0
Frederico Zolio <fredzolio@live.com>
2025-10-03 15:31:06 -0300

    feat: adicionar campo ID e atualizar estrutura da tabela CNAE
    - Adicionar o campo ID como chave primária na tabela CNAE, substituindo o campo código.
    - Atualizar a documentação OpenAPI e Swagger para incluir o novo campo ID.
    - Ajustar as referências e relacionamentos nas tabelas relacionadas ao sistema MEI, garantindo a integridade dos dados.
    - Remover validações manuais de duplicidade de CNAE, utilizando a constraint UNIQUE no banco de dados.

--- internal/models/cnae.go
+++ internal/models/cnae.go
@@ -1,13 +1,14 @@
 package models
 
 type CNAE struct {
-	Codigo   string `json:"codigo" gorm:"primaryKey;type:varchar(20)"`
+	ID       int    `json:"id" gorm:"primaryKey;autoIncrement"`
+	Codigo   string `json:"codigo" gorm:"type:varchar(20);not null;uniqueIndex:idx_codigo_servico"`
 	Ocupacao string `json:"ocupacao" gorm:"type:varchar(255);not null"`
-	Servico  string `json:"servico" gorm:"type:varchar(500);not null"`
+	Servico  string `json:"servico" gorm:"type:varchar(500);not null;uniqueIndex:idx_codigo_servico"`
 
 	// Relacionamentos
-	Oportunidades []OportunidadeMEI `json:"oportunidades,omitempty" gorm:"foreignKey:CNAECodigo" swaggerignore:"true"`
-	MEIEmpresas   []MEIEmpresa      `json:"-" gorm:"many2many:mei_empresas_cnaes;" swaggerignore:"true"`
+	Oportunidades []OportunidadeMEI `json:"oportunidades,omitempty" gorm:"foreignKey:CNAEID" swaggerignore:"true"`
+	MEIEmpresas   []MEIEmpresa      `json:"-" gorm:"many2many:mei_empresas_cnaes;joinForeignKey:cnae_id;joinReferences:mei_empresa_id" swaggerignore:"true"`
 }
 
 func (CNAE) TableName() string {
